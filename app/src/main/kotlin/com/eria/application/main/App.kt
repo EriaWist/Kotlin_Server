f6d267132698b75455667318977ff6b8b2c60112
Eria <c121680118@gmail.com>
2021-02-23 02:23:55 +0800

    可用gui簡單開啟server

--- app/src/main/kotlin/com/eria/application/main/App.kt
+++ app/src/main/kotlin/com/eria/application/main/App.kt
@@ -3,6 +3,10 @@
  */
 package com.eria.application.main
 
+import com.eria.http.MethodType
+import com.eria.http.Response
+import com.eria.http.createHttpServer
+import javafx.beans.property.SimpleStringProperty
 import javafx.collections.FXCollections
 import javafx.scene.control.TextField
 import tornadofx.*
@@ -17,18 +21,35 @@ fun main(args: Array<String>) {
 class MyView: View() {
     var portField: TextField by singleAssign()
     val texasCities = FXCollections.observableArrayList( "GET","POST")
+    val selectedCity = SimpleStringProperty()
+    var methodType:MethodType=MethodType.GET
     override val root = vbox {
         hbox {
             label("輸入port")
             portField = textfield()
         }
-        combobox<String> {
-            items = texasCities
-
-        }
+        combobox(selectedCity, texasCities)
         button(" 開始"){
-            action { }
+            action {
+                var http = createHttpServer()
+
+                    if (selectedCity.value == "GET") {
+                        methodType =MethodType.GET
+                    }else
+                    {
+                        methodType =MethodType.POST
+                    }
+                http.addRouting("/", methodType){ request ->
+                    var response = Response()
+                    response.body="test123"
+                    response
+                }
+                http.star(portField.text.toInt())
+
+            }
         }
 
     }
-}
+
+    }
+
