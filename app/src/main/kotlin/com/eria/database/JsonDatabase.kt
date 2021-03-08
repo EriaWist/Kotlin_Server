1eac85f9e3cb732e55011eb7c450647e2101f2c7
Eria <c121680118@gmail.com>
2021-03-08 14:52:21 +0800

    Json的資料庫

---
+++ app/src/main/kotlin/com/eria/database/JsonDatabase.kt
@@ -0,0 +1,13 @@
+package com.eria.database
+
+import kotlinx.serialization.*
+import kotlinx.serialization.json.*
+
+
+class JsonDatabase {
+    var database =""
+    fun addData(){}
+    fun savedata(){}
+    fun updateDatabase(){}
+
+}
